b11af39257a83711c87e34f993dc5fbb67f5d857
Chirag <118429338+Chirag423@users.noreply.github.com>
2024-09-02 22:49:15 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const key = 'eaWpu4kuSiZ3vVjIyckjxyMoM9nSDMEHgKwwV9O55Ryu7VsZMHPzZcY3Wribpf4p';
-export const secretKey ='sWWeFCzGU6hKEReaw1S4hRdflHYdxVVDgGwePS7V';
+export const key = '';
+export const secretKey ='';
